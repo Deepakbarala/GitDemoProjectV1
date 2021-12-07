32938e4cce3a8ea17ab906f2188a2ad302cfc702
Deepak Barala <deepakbarala25@gmail.com>
2021-12-07 16:25:30 +0530

    First commit

---
+++ GitDemoProjectV1/src/Default/Test.java
@@ -0,0 +1,15 @@
+package Default;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String hungry="Yes";
+		if(hungry=="yes") 
+			System.out.println("eat Something");
+		else
+			System.out.println("Do Work");
+	}
+
+}
